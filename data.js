d5b8ca840c26e3252beb056ab0c23f91d5573691
RBVC <sacra.motimoti+RVC@gmail.com>
2026-05-24 23:40:24 +0900

    up

--- data.js
+++ data.js
@@ -9,34 +9,31 @@ const V3_NEWS = [
 
 // V3 メンバー詳細
 const V3_MEMBERS = [
-    {
-        name: "JIA (지아)",
-        img: "images/v3/v3-jia.jpg",
-        birthday: "2001.10.09",
-        role: "Leader",
-        position: "Main Vocal / Sub Dancer / Sub Rapper"
-    },
-    {
-        name: "LICCA (릿카)",
-        img: "images/v3/v3-licca.jpg",
-        birthday: "2000.07.14",
-        role: "Center",
-        position: "Main Rapper / Lead Vocal / Lead Dancer"
-    },
-    {
-        name: "NAYEON (나연)",
-        img: "images/v3/v3-nayeon.jpg",
-        birthday: "2006.06.20",
-        role: "Maknae",
-        position: "Main Dancer / Lead Rapper / Sub Vocal"
-    }
+    { name: "JIA (지아)", img: "images/v3/v3-jia.jpg", birthday: "2001.10.09", role: "Leader", position: "Main Vocal / Sub Dancer / Sub Rapper" },
+    { name: "LICCA (릿카)", img: "images/v3/v3-licca.jpg", birthday: "2000.07.14", role: "Center", position: "Main Rapper / Lead Vocal / Lead Dancer" },
+    { name: "NAYEON (나연)", img: "images/v3/v3-nayeon.jpg", birthday: "2006.06.20", role: "Maknae", position: "Main Dancer / Lead Rapper / Sub Vocal" }
 ];
 
-// V3 アルバム (URLを embed に変換済み)
+// V3 アルバム (embedコードを直接記述する方式へ変更)
 const V3_ALBUMS = [
-    { title: "COOL", type: "2nd Mini Album / 2022.10.31", img: "images/v3/v3-cool.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0ZeknS5ZhOGKkPxGKq5MjJAI6JVPLpTXBeRV" },
-    { title: "VENOM", type: "1st Mini Album / 2022.04.10", img: "images/v3/v3-venom.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0Z8knS5ZA3xYWPiILQ0M3rQzVDNrM5a4Onw7" },
-    { title: "ALL OUT", type: "Pre-Debut Cover EP / 2022.04.01", img: "images/v3/v3-allout.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0ZzzoU5ZgjQk2aTBTHF0ig70v7BxL0Y0ddGX" }
+    { 
+        title: "COOL", 
+        type: "2nd Mini Album / 2022.10.31", 
+        img: "images/v3/v3-cool.jpg", 
+        embed: `<iframe src="https://u.pcloud.link/publink/embed?code=0ZeknS5ZhOGKkPxGKq5MjJAI6JVPLpTXBeRV" frameborder="0" width="100%" height="350" scrolling="no"></iframe>`
+    },
+    { 
+        title: "VENOM", 
+        type: "1st Mini Album / 2022.04.10", 
+        img: "images/v3/v3-venom.jpg", 
+        embed: `<iframe src="https://u.pcloud.link/publink/embed?code=0Z8knS5ZA3xYWPiILQ0M3rQzVDNrM5a4Onw7" frameborder="0" width="100%" height="350" scrolling="no"></iframe>`
+    },
+    { 
+        title: "ALL OUT", 
+        type: "Pre-Debut Cover EP / 2022.04.01", 
+        img: "images/v3/v3-allout.jpg", 
+        embed: `<iframe src="https://u.pcloud.link/publink/embed?code=0ZzzoU5ZgjQk2aTBTHF0ig70v7BxL0Y0ddGX" frameborder="0" width="100%" height="350" scrolling="no"></iframe>`
+    }
 ];
 
 // --- 共通機能 ---
@@ -47,36 +44,25 @@ function toggleMenu() {
     document.getElementById('menu-closer').classList.toggle('open');
 }
 
-// モーダル：アルバム用
 function openAlbumModal(i) {
     const a = V3_ALBUMS[i];
     document.getElementById('modal-title').innerText = a.title;
     document.getElementById('modal-body').innerHTML = `
-        <p style="font-size:0.9rem; color:#ccc; margin-bottom:15px; font-family:'Noto Sans JP';">${a.type}</p>
-        <iframe src="${a.pcloud}" frameborder="0" width="100%" height="350" scrolling="no"></iframe>
+        <p style="font-size:0.9rem; color:#ccc; margin-bottom:15px; font-family:'Noto Sans JP'; text-align:center;">${a.type}</p>
+        <div class="player-container">${a.embed}</div>
     `;
     document.getElementById('master-modal').classList.add('active');
 }
 
-// モーダル：メンバー用 (整列修正：画像２対応)
 function openMemberModal(i) {
     const m = V3_MEMBERS[i];
     document.getElementById('modal-title').innerText = m.name;
     document.getElementById('modal-body').innerHTML = `
         <img src="${m.img}" class="modal-img">
         <div class="modal-info-list">
-            <div class="modal-info-item">
-                <span class="modal-label">BIRTHDAY</span>
-                <span class="modal-value">${m.birthday}</span>
-            </div>
-            <div class="modal-info-item">
-                <span class="modal-label">ROLE</span>
-                <span class="modal-value">${m.role}</span>
-            </div>
-            <div class="modal-info-item">
-                <span class="modal-label">POSITION</span>
-                <span class="modal-value">${m.position}</span>
-            </div>
+            <div class="modal-info-item"><span class="modal-label">BIRTHDAY</span><span class="modal-value">${m.birthday}</span></div>
+            <div class="modal-info-item"><span class="modal-label">ROLE</span><span class="modal-value">${m.role}</span></div>
+            <div class="modal-info-item"><span class="modal-label">POSITION</span><span class="modal-value">${m.position}</span></div>
         </div>
     `;
     document.getElementById('master-modal').classList.add('active');

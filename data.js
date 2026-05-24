0a37f35db9414d9b0c9e744a323b6a6325d83d00
RBVC <sacra.motimoti+RVC@gmail.com>
2026-05-24 12:38:54 +0900

    up

--- data.js
+++ data.js
@@ -7,9 +7,9 @@ const V3_NEWS = [
 ];
 
 const V3_ALBUMS = [
-    { title: "COOL", type: "2nd Mini Album / 2022.10.31", img: "images/v3-cool.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0ZeknS5ZhOGKkPxGKq5MjJAI6JVPLpTXBeRV" },
-    { title: "VENOM", type: "1st Mini Album / 2022.04.10", img: "images/v3-venom.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0Z8knS5ZA3xYWPiILQ0M3rQzVDNrM5a4Onw7" },
-    { title: "ALL OUT", type: "Pre-Debut Cover EP / 2022.04.01", img: "images/v3-allout.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0ZzzoU5ZgjQk2aTBTHF0ig70v7BxL0Y0ddGX" }
+    { title: "COOL", type: "2nd Mini Album / 2022.10.31", img: "images/v3/v3-cool.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0ZeknS5ZhOGKkPxGKq5MjJAI6JVPLpTXBeRV" },
+    { title: "VENOM", type: "1st Mini Album / 2022.04.10", img: "images/v3/v3-venom.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0Z8knS5ZA3xYWPiILQ0M3rQzVDNrM5a4Onw7" },
+    { title: "ALL OUT", type: "Pre-Debut Cover EP / 2022.04.01", img: "images/v3/v3-allout.jpg", pcloud: "https://u.pcloud.link/publink/embed?code=0ZzzoU5ZgjQk2aTBTHF0ig70v7BxL0Y0ddGX" }
 ];
 
 // ハンバーガーメニュー

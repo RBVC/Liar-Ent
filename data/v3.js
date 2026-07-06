348e2147c06bdc206795b48ec8f08b05cc3da13a
RBVC <sacra.motimoti+RVC@gmail.com>
2026-07-06 23:43:42 +0900

    up

--- data/v3.js
+++ data/v3.js
@@ -4,21 +4,38 @@ window.allGroups["v3"] = {
     banner: "images/v3/v3-main.jpg",
     about: "<3（V3、ヴィスリ）は、韓国の3人組バーチャルアイドルグループ。グループ名は『ViVid Venom』の略称であり“鮮やかな音楽の毒で世界を魅了する”という意味を持つ。",
     members: [
-        { name: "JIA (ジア)", birthday: "2001.10.09", position: "Leader / Main Vocal / Sub Dancer / Sub Rapper", img: "images/v3/v3-jia.jpg" },
-        { name: "LICCA (リッカ)", birthday: "2000.07.14", position: "Center / Main Rapper / Lead Vocal / Lead Dancer", img: "images/v3/v3-licca.jpg" },
-        { name: "NAYEON (ナヨン)", birthday: "2006.06.20", position: "Maknae / Main Dancer / Lead Rapper / Sub Vocal", img: "images/v3/v3-nayeon.jpg" }
+        { 
+            name: "JIA (ジア)", 
+            birthday: "2001.10.09", 
+            position: "Leader / Main Vocal / Sub Dancer / Sub Rapper", 
+            img: "images/v3/v3-jia.jpg" 
+        },
+        { 
+            name: "LICCA (リッカ)", 
+            birthday: "2000.07.14", 
+            position: "Center / Main Rapper / Lead Vocal / Lead Dancer", 
+            img: "images/v3/v3-licca.jpg" 
+        },
+        { 
+            name: "NAYEON (ナヨン)", 
+            birthday: "2006.06.20", 
+            position: "Maknae / Main Dancer / Lead Rapper / Sub Vocal", 
+            img: "images/v3/v3-nayeon.jpg" 
+        }
     ],
     news: [
         { date: "2023.02.28", title: "[INFO] 今後の活動および新グループ合流に関する重要なお知らせ" },
         { date: "2022.11.05", title: "[RELEASE] 2nd Mini Album『Cool』リリースのお知らせ" },
         { date: "2022.05.13", title: "[RELEASE] 1st Mini Album『VENOM』リリースのお知らせ" },
         { date: "2022.04.10", title: "[INFO] プレデビューカバーEP『ALL OUT』公開のお知らせ" },
+        { date: "2022.04.01", title: "[INFO] Liar Entertainment 新グループ「<3」結成に関するお知らせ" },
         { date: "2022.04.01", title: "[INFO] グループ名・メンバー公開" }
     ],
     discography: [
-        { title: "COOL", type: "2nd Mini Album / 2022.10.31", img: "images/v3/v3-cool.jpg", embedUrl: "https://u.pcloud.link/publink/show?code=0ZeknS5ZhOGKkPxGKq5MjJAI6JVPLpTXBeRV&view=list" },
-        { title: "VENOM", type: "1st Mini Album / 2022.04.10", img: "images/v3/v3-venom.jpg", embedUrl: "https://u.pcloud.link/publink/show?code=0Z8knS5ZA3xYWPiILQ0M3rQzVDNrM5a4Onw7&view=list" },
-        { title: "ALL OUT", type: "Pre-Debut Cover EP / 2022.04.01", img: "images/v3/v3-allout.jpg", embedUrl: "https://u.pcloud.link/publink/show?code=0ZzzoU5ZgjQk2aTBTHF0ig70v7BxL0Y0ddGX&view=list" }
+        { title: "COOL", type: "2nd Mini Album / 2022.11.05", img: "images/v3/v3-cool.jpg", embedUrl: "https://u.pcloud.link/publink/show?code=0ZeknS5ZhOGKkPxGKq5MjJAI6JVPLpTXBeRV&view=list" },
+        { title: "VENOM", type: "1st Mini Album / 2022.05.13", img: "images/v3/v3-venom.jpg", embedUrl: "https://u.pcloud.link/publink/show?code=0Z8knS5ZA3xYWPiILQ0M3rQzVDNrM5a4Onw7&view=list" },
+        { title: "ALL OUT", type: "Pre-Debut Cover EP / 2022.04.10", img: "images/v3/v3-allout.jpg", embedUrl: "https://u.pcloud.link/publink/show?code=0ZzzoU5ZgjQk2aTBTHF0ig70v7BxL0Y0ddGX&view=list" }
     ],
-    hasMemberSection: true, clickableMembers: true
+    hasMemberSection: true, 
+    clickableMembers: true
 };

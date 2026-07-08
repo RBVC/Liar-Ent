cbdced42347aee20b8a6dca3f3516b448a3f1cd0
RBVC <sacra.motimoti+RVC@gmail.com>
2026-07-08 22:37:44 +0900

    up

--- data/v3.js
+++ data/v3.js
@@ -4,9 +4,24 @@ window.allGroups["v3"] = {
     banner: "images/v3/v3-main.jpg",
     about: "<3（V3、ヴィスリ）は、韓国の3人組バーチャルアイドルグループ。グループ名は『ViVid Venom』の略称であり“鮮やかな音楽の毒で世界を魅了する”という意味を持つ。",
     members: [
-        { name: "JIA (ジア)", birthday: "2001.10.09", position: "Leader / Main Vocal / Sub Dancer / Sub Rapper", img: "images/v3/v3-jia.jpg" },
-        { name: "LICCA (リッカ)", birthday: "2000.07.14", position: "Center / Main Rapper / Lead Vocal / Lead Dancer", img: "images/v3/v3-licca.jpg" },
-        { name: "NAYEON (ナヨン)", birthday: "2006.06.20", position: "Maknae / Main Dancer / Lead Rapper / Sub Vocal", img: "images/v3/v3-nayeon.jpg" }
+        { 
+            name: "JIA (ジア)", 
+            birthday: "2001.10.09", 
+            position: "Leader / Main Vocal / Sub Dancer / Sub Rapper", 
+            img: "images/v3/v3-jia.jpg" 
+        },
+        { 
+            name: "LICCA (リッカ)", 
+            birthday: "2000.07.14", 
+            position: "Center / Main Rapper / Lead Vocal / Lead Dancer", 
+            img: "images/v3/v3-licca.jpg" 
+        },
+        { 
+            name: "NAYEON (ナヨン)", 
+            birthday: "2006.06.20", 
+            position: "Maknae / Main Dancer / Lead Rapper / Sub Vocal", 
+            img: "images/v3/v3-nayeon.jpg" 
+        }
     ],
     news: [
         { date: "2023.02.28", title: "[INFO] 今後の活動および新グループ合流に関する重要なお知らせ" },
@@ -17,8 +32,10 @@ window.allGroups["v3"] = {
     ],
     discography: [
         { 
-            title: "COOL", type: "2nd Mini Album / 2022.11.05", img: "images/v3/v3-cool.jpg",
-            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/RBVC/Liar%20Entertainment/V3/Cool/",
+            title: "COOL", 
+            type: "2nd Mini Album / 2022.11.05", 
+            img: "images/v3/v3-cool.jpg",
+            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/v1783516704/",
             tracks: [
                 { title: "BE!", url: "01_BE.m4a" },
                 { title: "Cool", url: "02_Cool.m4a" },
@@ -29,8 +46,10 @@ window.allGroups["v3"] = {
             ]
         },
         { 
-            title: "VENOM", type: "1st Mini Album / 2022.05.13", img: "images/v3/v3-venom.jpg",
-            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/RBVC/Liar%20Entertainment/V3/VENOM/",
+            title: "VENOM", 
+            type: "1st Mini Album / 2022.05.13", 
+            img: "images/v3/v3-venom.jpg",
+            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/v1783516704/",
             tracks: [
                 { title: "Re-Original", url: "01_Re-Original.m4a" },
                 { title: "VENOM", url: "02_VENOM.m4a" },
@@ -41,8 +60,10 @@ window.allGroups["v3"] = {
             ]
         },
         { 
-            title: "ALL OUT", type: "Pre-Debut Cover EP / 2022.04.10", img: "images/v3/v3-allout.jpg", 
-            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/RBVC/Liar%20Entertainment/V3/ALL%20OUT/",
+            title: "ALL OUT", 
+            type: "Pre-Debut Cover EP / 2022.04.10", 
+            img: "images/v3/v3-allout.jpg", 
+            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/v1783516705/",
             tracks: [
                 { title: "THE BADDEST", url: "01_THE_BADDEST.m4a" },
                 { title: "I'LL SHOW YOU (Jia Solo)", url: "02_I_LL_SHOW_YOU.m4a" },

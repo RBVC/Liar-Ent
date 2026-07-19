98fb20815953dcbbf2e0326bb5cb106b0cd83e03
RBVC <sacra.motimoti+RVC@gmail.com>
2026-07-19 12:19:53 +0900

    Update hayul.js

--- data/hayul.js
+++ data/hayul.js
@@ -2,9 +2,35 @@
 window.allGroups["hayul"] = {
     name: "HAYUL",
     banner: "images/hayul/hayul-main.jpg",
-    about: "HAYUL（ハユル）は、GØD7IKEのメインボーカル。2024年2月、デジタルシングル『神谕法则』で待望のソロデビューを果たした。透明感のある歌声と幻想的なビジュアルで、独自の音楽世界を構築する。",
+    about: "HAYUL（ハユル）は、GØD7IKEのメインボーカル。2024年2月、デジタルシングル『Just Like a Dream』で待望 of ソロデビューを果たした。ソロ三部作を通して描かれる「光の天使から、闇の堕天使へ」という物語は、グループ全体の神話とも深く連動しており、透明感あふれる歌声から変貌の予兆を感じさせる表現まで、独自の音楽世界を構築している。",
     members: [],
-    news: [ { date: "2024.02.07", title: "[RELEASE] デジタルシングル『神谕法则』リリースのお知らせ" } ],
-    discography: [ { title: "神谕法则", type: "1st Digital Single / 2024.02.07", img: "images/hayul/hayul-the-law-of-god.jpg", tracks: [] } ],
+    news: [
+        { date: "2025.06.24", title: "[RELEASE] 3rd Digital Single『HAWWAH』リリースのお知らせ" },
+        { date: "2024.08.12", title: "[RELEASE] 2nd Digital Single『神谕法则』リリースのお知らせ" },
+        { date: "2024.02.07", title: "[RELEASE] 1st Digital Single『Just Like a Dream』リリースのお知らせ" }
+    ],
+    discography: [
+        { 
+            title: "HAWWAH", 
+            type: "3rd Digital Single / 2025.06.24", 
+            img: "images/hayul/hawwah.jpg", 
+            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/",
+            tracks: [ { title: "HAWWAH", url: "HAWWAH.m4a" } ]
+        },
+        { 
+            title: "神谕法则", 
+            type: "2nd Digital Single / 2024.08.12", 
+            img: "images/hayul/the-law-of-god.jpg", 
+            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/",
+            tracks: [ { title: "神谕法则", url: "The_Law_of_God.m4a" } ]
+        },
+        { 
+            title: "Just Like a Dream", 
+            type: "1st Digital Single / 2024.02.07", 
+            img: "images/hayul/just-like-a-dream.jpg", 
+            baseUrl: "https://res.cloudinary.com/udhnprle/video/upload/",
+            tracks: [ { title: "Just Like a Dream", url: "Just_Like_a_Dream.m4a" } ]
+        }
+    ],
     hasMemberSection: false
 };
